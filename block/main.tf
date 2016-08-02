177ad0dee95028faef921347be421a69fe5814ca
Israel Sotomayor <sotoisra24@gmail.com>
2016-08-02 12:47:37 +0200

    fix: typo on blob type

--- block/main.tf
+++ block/main.tf
@@ -5,5 +5,5 @@ resource "azurerm_storage_blob" "mod" {
     storage_account_name = "${var.storage_account_name}"
     storage_container_name = "${var.storage_container_name}"
 
-    type = "blob"
+    type = "block"
 }
